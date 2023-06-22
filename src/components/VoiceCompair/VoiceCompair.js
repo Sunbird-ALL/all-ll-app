5ddb97f9ab804e27881d72c7af63254ae86de29c
vaibhavsTekdi <vaibhav_s@tekditechnologies.com>
2023-06-22 12:28:54 +0530

    Issue #5 feat: Capture Sentence results [Telemetry Enhancements]

--- src/components/VoiceCompair/VoiceCompair.js
+++ src/components/VoiceCompair/VoiceCompair.js
@@ -109,6 +109,36 @@ const VoiceCompair = props => {
       body: payload,
       redirect: 'follow',
     };
+    function replaceAll(string, search, replace) {
+        return string.split(search).join(replace);
+    }
+    function compareArrays(arr1, arr2) {
+        let words_result = [];
+
+        // Iterate over each element and compare
+        for (var k in arr1){
+            if (arr2[k] == "" || arr2[k] == undefined)
+            {
+                // Element not available on the same key
+                words_result.push('-1');
+            }
+            else if (arr1[k] === arr2[k]) {
+                // Elements match on the same key
+                words_result.push('1');
+            }
+            else {
+                // Element does not match on the same key
+                words_result.push('0');
+            }
+        }
+        if (arr1.length < arr2.length)
+        {
+           for (let i = arr1.length; i < arr2.length; i++){
+              words_result.push('-1');
+           }
+        }
+        return words_result;
+    }
     const apiURL = `${ASR_REST_URLS[sourceLanguage]}/asr/v1/recognize/${sourceLanguage}`;
     const responseStartTime = new Date().getTime();
     await fetch(apiURL, requestOptions)
@@ -117,12 +147,73 @@ const VoiceCompair = props => {
         const responseEndTime = new Date().getTime();
         const responseDuration = Math.round((responseEndTime - responseStartTime) / 1000);
         var apiResponse = JSON.parse(result);
+
+        // Data Manipulation on result capturing for telemetry log
+        let texttemp = apiResponse['output'][0]['source'].toLowerCase();
+        const studentTextArray = texttemp.split(' ');
+
+        let tempteacherText = localStorage.getItem('contentText').toLowerCase();
+        tempteacherText = replaceAll(tempteacherText, '.', '');
+        tempteacherText = replaceAll(tempteacherText, "'", '');
+        tempteacherText = replaceAll(tempteacherText, ',', '');
+        tempteacherText = replaceAll(tempteacherText, '!', '');
+        tempteacherText = replaceAll(tempteacherText, '|', '');
+        const teacherTextArray = tempteacherText.split(' ');;
+
+        let student_correct_words_result = [];
+        let student_incorrect_words_result = [];
+        let originalwords = teacherTextArray.length;
+        let studentswords = studentTextArray.length;
+        let wrong_words = 0;
+        let correct_words = 0;
+        let result_per_words = 0;
+        let result_per_words1 = 0;
+        let occuracy_percentage = 0;
+
+        let word_result_array = compareArrays(teacherTextArray, studentTextArray);
+
+        for (let i = 0; i < studentTextArray.length; i++) {
+            if (teacherTextArray.includes(studentTextArray[i])) {
+               correct_words++;
+               student_correct_words_result.push(
+                  studentTextArray[i]
+               );
+            } else {
+                wrong_words++;
+                student_incorrect_words_result.push(
+                   studentTextArray[i]
+                );
+            }
+        }
+        //calculation method
+        if (originalwords >= studentswords) {
+           result_per_words = Math.round(
+                 Number((correct_words / originalwords) * 100)
+           );
+        } else {
+            result_per_words = Math.round(
+              Number((correct_words / studentswords) * 100)
+            );
+        }
+ 
+        let word_result = (result_per_words == 100) ? "correct" : "incorrect";
+
         response({ // Required
             "target": localStorage.getItem('contentText'), // Required. Target of the response
             "qid": "", // Required. Unique assessment/question id
             "type": "SPEAK", // Required. Type of response. CHOOSE, DRAG, SELECT, MATCH, INPUT, SPEAK, WRITE
-            "values": [{ "original_text": localStorage.getItem('contentText') },{ "response_text": apiResponse['output'][0]['source']}, { "duration":  responseDuration}] // Required. Array of response tuples. For ex: if lhs option1 is matched with rhs optionN - [{"lhs":"option1"}, {"rhs":"optionN"}]
+            "values": [
+                { "original_text": localStorage.getItem('contentText') },
+                { "response_text": apiResponse['output'][0]['source']},
+                { "response_correct_words_array": student_correct_words_result},
+                { "response_incorrect_words_array": student_incorrect_words_result},
+                { "response_word_array_result": word_result_array},
+                { "response_word_result": word_result},
+                { "accuracy_percentage": result_per_words},
+                { "duration":  responseDuration}
+             ]
         })
+
         setAi4bharat(
           apiResponse['output'][0]['source'] != ''
             ? apiResponse['output'][0]['source']
