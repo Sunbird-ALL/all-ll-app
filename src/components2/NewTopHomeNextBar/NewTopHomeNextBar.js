570cb727c6b86591b1cf1a31f9e0b806a765a5ba
Ajinkya Pande <ajinkya.pande@tekditechnologies.com>
2023-06-24 16:14:01 +0530

    work_done_on_paragraph_hide

--- src/components2/NewTopHomeNextBar/NewTopHomeNextBar.js
+++ src/components2/NewTopHomeNextBar/NewTopHomeNextBar.js
@@ -15,10 +15,11 @@ const NewTopHomeNextBar = props => {
       <div className="row">
         {ishomeback ? (
           <>
-            <div className="col s6 home_icon_div">
-              <Link to={'/proto2/'}>
+            <div className="col s6 home_icon_div" onClick={() => navigate(-1)}>
+              {/* <Link to={'/proto2/'}> */}
+              
                 <img src={home} className="home_icon_new"></img>
-              </Link>
+              {/* </Link> */}
             </div>
             <div className="col s6 menu_icon_div">
               {/* <Link to={'/proto2/start'}>
@@ -28,7 +29,7 @@ const NewTopHomeNextBar = props => {
           </>
         ) : props.isHideNavigation ? (
           <>
-            <div className="col s6 home_icon_div" onClick={() => navigate(-1)}>
+            <div className="col s6 home_icon_div" onClick={() => navigate(-2)}>
               {/* <Link to={`${localStorage.getItem('URL')}`}> */}
               <img src={home} className="home_icon_new"></img>
               {/* </Link> */}
