2c25383444f5fbef62fa1ce332cda00ed678b8c3
Ajinkya Pande <ajinkya.pande@tekditechnologies.com>
2023-09-18 16:36:09 +0530

    remove comma from parameter

--- src/services/callTelemetryIntract.js
+++ src/services/callTelemetryIntract.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { interact,start } from '../services/telementryService';
 const duration = new Date().getTime();
 
-export const interactCall = (id, uri, telemetryMode, currentPage, ) => {
+export const interactCall = (id, uri, telemetryMode, currentPage) => {
   interact(id, uri, telemetryMode, currentPage)
 };
 
