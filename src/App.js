77231813d2729e887e480cfc5393ae9f5ed7668a
Ajinkya Pande <ajinkya.pande@tekditechnologies.com>
2024-11-08 14:30:27 +0530

    Issueid #230114 feat: accessing data from morigal to cloudfront

--- src/App.js
+++ src/App.js
@@ -77,31 +77,21 @@ function App() {
   }, []);
 
   useEffect(() => {
-
     const handleMessage = (event) => {
-
-
       // Destructure the message data
-      const { token, buddyToken, messageType, contentSessionId } = event.data;
+      const { messageType, localStorageKeyValue } = event.data;
+      if (messageType === "customData") {
+        for (const item of localStorageKeyValue) {
+          const key = item.key;
+          const value = item.value;
 
-      // Check if the expected data exists
-      if (messageType === 'customData') {
-        if (token) {
-          localStorage.setItem('token', token);
-        }
-        if (buddyToken) {
-          localStorage.setItem('buddyToken', buddyToken);
-        }
-        if (contentSessionId) {
-          localStorage.setItem('contentSessionId', contentSessionId);
+          localStorage.setItem(key, value);
         }
       }
     };
-
-    window.addEventListener('message', handleMessage);
-
+    window.addEventListener("message", handleMessage);
     return () => {
-      window.removeEventListener('message', handleMessage);
+      window.removeEventListener("message", handleMessage);
     };
   }, []);
 
